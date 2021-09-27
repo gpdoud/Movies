2cbf057cfe289fec1cbd707b4ba0a34b4dca4e62
Greg Doud <gpdoud@gmail.com>
2021-09-27 16:38:58 -0400

    Add project files.

---
+++ Movies/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+	class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Genre { get; set; }
+		public string Rating { get; set; } = "PG";
+		public int Released { get; set; } // year only
+		public int RuntimeInMinutes { get; set; }
+		public string Director { get; set; }
+
+		public Movie() { }
+		public Movie(int id, string title, string genre, string rating,
+					int released, int runtimeInMinutes, string director)
+		{
+			this.Id = id;
+			this.Title = title;
+			this.Genre = genre;
+			this.Rating = rating;
+			this.Released = released;
+			this.RuntimeInMinutes = runtimeInMinutes;
+			this.Director = director;
+		}
+	}
+}
